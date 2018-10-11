4558c01492f6e6851685f225fa1d96837cc5b954
Paul Bernitz <paulbernitz@web.de>
2018-10-11 18:46:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
-#include <algorithm>
-#include <cctype>
-#include <iostream>
-#include <string>
-#include <vector>
+#include "hello-world.hpp"
  
 /*
 * Testing C++17 features for TravisCI.
 */
 int main()
 {
-    std::string s("hello");
-    std::transform(s.begin(), s.end(), s.begin(),
-                   [](unsigned char c) -> unsigned char { return std::toupper(c); });
- 
-    std::vector<std::size_t> ordinals;
-    std::transform(s.begin(), s.end(), std::back_inserter(ordinals),
-                   [](unsigned char c) -> std::size_t { return c; });
- 
-    std::cout << s << ':';
-    for (auto ord : ordinals) {
-       std::cout << ' ' << ord;
-    }
+  hello_world();
+  
+  return 0;
 }

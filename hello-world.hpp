408453ae103fdfe5f034cedfa32d40e4afa0f53e
Paul Bernitz <paulbernitz@web.de>
2018-10-11 19:19:12 +0200

    travis CI should fail.

--- hello-world.hpp
+++ hello-world.hpp
@@ -6,4 +6,6 @@ void hello_world()
     // Jason Turner https://github.com/lefticus
     // deserves credit for this creative way to test for C++17: 
     [out = std::ref(std::cout << "Hello ")](){ out.get() << "World\n"; }();
+    
+    duck(); // This should fail :^)
 }

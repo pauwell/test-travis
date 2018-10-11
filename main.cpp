5ca6f0c3d7e6d7d0160b8d6f2bc9ead889ed9272
Paul Bernitz <paulbernitz@web.de>
2018-10-11 08:32:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
+#include <algorithm>
+#include <cctype>
 #include <iostream>
-
-int main() 
+#include <string>
+#include <vector>
+ 
+/*
+* Testing C++17 features for TravisCI.
+*/
+int main()
 {
-	std::cout << "Im NOT C++17!" << '\n';
-
-	return 0;
+    std::string s("hello");
+    std::transform(s.begin(), s.end(), s.begin(),
+                   [](unsigned char c) -> unsigned char { return std::toupper(c); });
+ 
+    std::vector<std::size_t> ordinals;
+    std::transform(s.begin(), s.end(), std::back_inserter(ordinals),
+                   [](unsigned char c) -> std::size_t { return c; });
+ 
+    std::cout << s << ':';
+    for (auto ord : ordinals) {
+       std::cout << ' ' << ord;
+    }
 }

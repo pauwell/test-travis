be957d534a4d9aeda4e095c6b25dc0a6d2a53ad9
Paul Bernitz <paulbernitz@web.de>
2018-10-11 18:43:41 +0200

    Create hello-world.hpp

---
+++ hello-world.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <functional>
+
+void hello_world()
+{
+    // Jason Turner https://github.com/lefticus
+    // deserves credit for this creative way to test for C++17: 
+    [out = std::ref(std::cout << "Hello ")](){ out.get() << "World\n"; }();
+}
